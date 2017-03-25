707603c0ea375f1578a648dc5f953e29b1225c48
Guilherme Santos <sntguilherme@gmail.com>
2017-03-25 17:28:47 -0300

    Exercises of the instruction 3

--- ANNOTATIONS.md
+++ ANNOTATIONS.md
@@ -1,6 +1,10 @@
-- Locale Compare (comparar Strings acentuadas);
-- Symbol - Identificar a propriedade de um objeto (pode promover a privacidade e pevenir a sobreescrita);
-- for of para arrays ou objetos.
-- Object.defineProperty
-- Object.seal (writable = true, resto false)
-- Object.freeze (tudo false)
+# Anotações
+
+* Locale Compare (comparar Strings acentuadas);
+* Symbol - Identificar a propriedade de um objeto (pode promover a privacidade e pevenir a sobreescrita);
+* for of para arrays ou objetos.
+* Object.defineProperty
+* Object.seal (writable = true, resto false)
+* Object.freeze (tudo false)
+* get e set no atributo do object (encapsulamento), depois com proxy
+* atribuindo valores diretamente a variáveis com conversão de nomes
--- exercises/student/main.js
+++ exercises/student/main.js
@@ -1,22 +1,45 @@
-const SQL_COMMAND = 'create table author (id number, name string, city string, state string, country string)',
-      extractedCommand = SQL_COMMAND.match(/^create table ([a-z]+) \(([a-z\s,]+)\)$/),
-      tableName = extractedCommand[1],
-      columns = extractedCommand[2].split(/,\s?/),
-      database = {
-        tables: {
-          [tableName]: {
-            columns: {},
-            data: []
-          }
-        }
-      };
+const database = {
+  tables: {},
+  execute(command){
+    if (command.startsWith('create table')) {
+      return this.createTable(command);
+    }
 
-Object.assign(database.tables[tableName].columns,
-  ...columns.map(column => {
-    const columnExtracted = column.split(/\s/);
+    if (command.startsWith('insert into')) {
+      return this.insert(command);
+    }
+  },
+  createTable(createCommand){
+    const regex = /^create table ([a-z]+) \(([a-z\s,]+)\)$/;
+    let [, tableName, columns] = createCommand.match(regex);
+    columns = columns.split(/,\s?/);
 
-    return {[columnExtracted[0]]: columnExtracted[1]};
-  }));
+    this.tables[tableName] = {
+      columns: Object.assign({}, ...columns.map(column => {
+        const [key, value] = column.split(/\s/);
+
+        return {[key]: value};
+      })),
+      data: []
+    };
+  },
+  insert(insertCommand){
+    const regex = /^insert into ([a-z]+) \(([a-z\s,]+)\) values \(([\w\s,]+)\)$/;
+    let [, tableName, columns, values] = insertCommand.match(regex);
+    columns = columns.split(/,\s?/);
+    values = values.split(/,\s?/);
+
+    let insertObject = {};
+
+    for (let index = 0; index < columns.length; index++) {
+      Object.assign(insertObject, {
+        [columns[index]]: values[index]
+      });
+    }
+
+    this.tables[tableName].data.push(insertObject);
+  }
+};
 
 Object.defineProperty(database, 'tables', {
   writable: false,
@@ -24,6 +47,9 @@ Object.defineProperty(database, 'tables', {
   enumerable: true
 });
 
-delete database.tables;
-console.log(JSON.stringify(database));
+database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
+database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
 
+console.log(JSON.stringify(database));
+//console.log(JSON.stringify(database, null, 2));
